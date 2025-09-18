73ffd7e91cfbad286f5108866376cca74951344c
Bailey Miles <baileyleah542@gmail.com>
2025-09-18 02:35:31 +0100

    its 3am idfk what i changed something with the rotation or something idk  and project revamp weapson and stuff so they can work better or something idl

--- Scenes/Turrets/GunT1.gd
+++ Scenes/Turrets/GunT1.gd
@@ -2,13 +2,49 @@ extends Node2D
 
 var Bullet = preload("res://Scenes/Turrets/Missile.tscn")
 var bullet_damage = 5
-var path_name
-var curr_targets = []
+@onready var aim: Marker2D = $Turret/Marker2D
+@onready var turret: Sprite2D = $Turret
+var enemies_in_range = []
 var curr
 
-func _on_tower_radius_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+func _physics_process(delta: float) -> void:
+	# Update current target to the enemy with most progress
+	_update_current_target()
+	
+	if is_instance_valid(curr):
+		var direction = turret.global_position.direction_to(curr.global_position)
+		turret.rotation = direction.angle() + deg_to_rad(90)
+	
+func _update_current_target():
+	# Remove invalid enemies from the list
+	enemies_in_range = enemies_in_range.filter(func(enemy): return is_instance_valid(enemy))
+	
+	if enemies_in_range.is_empty():
+		curr = null
+		return
+	
+	# Find enemy with highest progress ratio
+	var best_enemy = null
+	var highest_progress = -1.0
+	
+	for enemy in enemies_in_range:
+		if enemy.is_in_group("soldier"):
+			var progress = enemy.get_parent().get_progress_ratio()
+			if progress > highest_progress:
+				highest_progress = progress
+				best_enemy = enemy
+	
+	curr = best_enemy
+
 
 func _on_tower_radius_entered(body: Node2D) -> void:
-	if "Soldier A" in body.name:
-		pass
+	if body.is_in_group("soldier"):
+		enemies_in_range.append(body)
+		var temp_bullet = Bullet.instantiate()
+		get_node("BulletContainer").call_deferred("add_child", temp_bullet)
+		temp_bullet.bullet_damage = bullet_damage
+		temp_bullet.global_position = aim.global_position
+
+func _on_tower_radius_exited(body: Node2D) -> void:
+	if body.is_in_group("soldier"):
+		enemies_in_range.erase(body)
--- Scenes/Turrets/Missile.gd
+++ Scenes/Turrets/Missile.gd
@@ -1,1 +1,44 @@
 extends CharacterBody2D
+
+@export var speed: float = 400
+var target_node: Node2D = null
+var bullet_damage = 2
+
+func _ready() -> void:
+	target_node = _find_closest_soldier()
+
+func _physics_process(delta: float) -> void:
+	if not is_instance_valid(target_node):
+		target_node = _find_closest_soldier()
+
+	if is_instance_valid(target_node):
+		var direction = global_position.direction_to(target_node.global_position)
+		velocity = direction * speed
+		rotation = direction.angle()
+		move_and_slide()
+	else:
+		call_deferred("queue_free")
+
+func _find_closest_soldier() -> Node2D:
+	var path_spawner = get_tree().root.get_node_or_null("Map1/PathSpawner")
+	if not path_spawner:
+		return null
+	
+	var closest_soldier = null
+	var closest_distance = INF
+	
+	# Get all soldiers in the scene tree
+	var soldiers = get_tree().get_nodes_in_group("soldier")
+	for soldier in soldiers:
+		if soldier is Node2D:
+			var distance = global_position.distance_to(soldier.global_position)
+			if distance < closest_distance:
+				closest_distance = distance
+				closest_soldier = soldier
+	
+	return closest_soldier
+
+func _on_area_2d_entered(body: Node2D) -> void:
+	if body.is_in_group("soldier"):
+		body.health -= bullet_damage
+		queue_free()
